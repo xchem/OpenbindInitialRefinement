1dc25375a55d497ef362dd99ff3d005cbf019b24
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-08-06 09:54:44 +0100

    First

--- src/openbind_initial_refinement/pre_export.py
+++ src/openbind_initial_refinement/pre_export.py
@@ -197,15 +197,16 @@ def drop_atoms(st, resids: list[ResID]) -> gemmi.Structure:
     new_st = st.clone()
     for chain in st[0]:
         del new_st[0][chain.name]
-        
+
+    resids_hashable = [(resid.chain, resid.seqid) for resid in resids]
+    
     for chain in st[0]:
         new_chain = gemmi.Chain(chain.name)
         for res in chain:
-            for resid in resids:
-                if (chain.name == resid.chain) & (str(res.seqid.num) == resid.seqid):
+            if (chain.name, str(res.seqid.num)) in resids_hashable:
                     continue
-                else:
-                    new_chain.add_residue(res)
+            else:
+                new_chain.add_residue(res)
         new_st[0].add_chain(new_chain)
     
     return new_st
